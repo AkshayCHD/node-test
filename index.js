4dce168d75637d4feef67bc24003d1b5add5b227
Akshay Kumar <email.akshay98@gmail.com>
2020-12-18 18:24:30 +0530

    Review for webhook

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/health-check', (req, res) => {
     console.log("Health checked at", (new Date()).toISOString());
-    res.send({ message: "Health checked, file syntax fixed", time: (new Date()).toISOString() })
+    res.send({ message: "Health checked, review by Praneeth", time: (new Date()).toISOString() })
 })
 
 app.listen(5000, () => {
